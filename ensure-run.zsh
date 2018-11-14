3ae3b0c92ebd332341f25d864a92e7268102caae
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2018-11-14 17:02:27 +0330

    Repo initialized. The software seems to work.

---
+++ ensure-run.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+myInvocation="$(printf %q "${(%):-%x}")$((($#)) && printf ' %q' "$@")"
+#echo "$myInvocation"
+eval "gtimeout $1 ${2}"
+res=$?
+# echo $res
+if [[ $res -eq 124 ]]; then
+    >&2 echo
+    >&2 echo "Command '$2' timed out in $1. Rerunning ..."
+    eval "$myInvocation"
+fi
